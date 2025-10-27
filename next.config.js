b03b4ace4104166fa5af6611013a648e8ffa62cf
andrew sciametta <eggystrone88@gmail.com>
2025-10-27 15:35:26 -0700

    Disable ESLint during production builds

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['i.scdn.co', 'mosaic.scdn.co'],
   },
+  eslint: {
+    // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
